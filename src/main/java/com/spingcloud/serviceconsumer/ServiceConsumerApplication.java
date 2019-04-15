50c829d0b702fbce0fac23642526f52dd2d1414e
cwn132 <19887460@qq.com>
2019-04-15 17:29:18 +0800

    222

--- src/main/java/com/spingcloud/serviceconsumer/ServiceConsumerApplication.java
+++ src/main/java/com/spingcloud/serviceconsumer/ServiceConsumerApplication.java
@@ -3,10 +3,13 @@ package com.spingcloud.serviceconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+//添加@EnableFeignClients注解
+@EnableFeignClients
 public class ServiceConsumerApplication {
 
     public static void main(String[] args) {
